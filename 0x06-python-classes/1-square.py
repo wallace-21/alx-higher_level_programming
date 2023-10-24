4e19b2c895bfd31ef3c0a70230bae48ddd2b4db8
root <root@Wallace.localdomain>
2023-10-25 01:40:44 +0200

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Square module."""
+
+
+
+class Square:
+
+    """Defines a square."""
+
+    def _init_(self, size):
+
+        """This is the constructor (init method)
+
+        Args:
+
+            size: size of a square
+
+        """
+
+        self.__size = size
