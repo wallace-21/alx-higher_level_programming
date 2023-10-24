8699397b404006556789fcf3cce4b8a1e6bcc825
root <root@Wallace.localdomain>
2023-10-24 17:25:45 +0200

    don't know I changed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 def uppercase(str):
     for i in str:
-        if 'a' <= i <= 'z':
+        if (ord("a") <= ord(i) <= ord("z")):
             i = chr(ord(i) - 32)
-        print(i, end="")
+
+        print("{:s}".format(i), end="")
 
     print()
