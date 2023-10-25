e449e6045727d78fab6e842cd18221d6048b67c8
root <root@Wallace.localdomain>
2023-10-25 02:21:11 +0200

    3. Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+
+    """Represent a square."""
+
+    def __init__(self, size=0):
+
+        """A new square.
+
+        Args:
+
+            size (int): The size of the new square.
+
+        """
+
+        if not isinstance(size, int):
+
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+
+        """Returns the current square area"""
+
+        return (self.__size ** 2)
