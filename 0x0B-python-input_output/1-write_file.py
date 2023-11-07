1b94aec75cd1f295852d1ff332ce73699cb0bd33
wallace-21 <exnesswallace>
2023-11-08 00:04:22 +0200

    huh

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,11 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """  function that writes a string to a text file
+    """  function that writes a string to a text file """
 
-    Args:
-        filename: name of the file being used
-        text: text to be added
-    """
     with open("filename", "w", encoding='utf-8') as file:
         return file.write(text)
