45d5e211cb03720ce04a257ce6220fa7b2bf2b1f
wallace-21 <exnesswallace>
2023-11-08 00:09:06 +0200

    wrong parenthesis

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """  function that writes a string to a text file """
 
-    with open("filename", "w", encoding='utf-8') as file:
+    with open(filename, "w", encoding='utf-8') as file:
         return file.write(text)
