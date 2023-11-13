b7ac76b4a0bccaeaa1309c52f8761d082a4c06be
wallace-21 <exnesswallace>
2023-11-13 22:36:43 +0200

    def update(self, *args): that assigns an argument to each attribute2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -55,6 +55,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
 
         super().__init__(id)
+
         self.__width = width
         self.__height = height
         self.__x = x
@@ -68,7 +69,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
 
             raise TypeError("width must be an integer")
 
@@ -87,7 +88,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
 
-        if not isinstance(value, int):
+        if type(value) is not int:
 
             raise TypeError("height must be an integer")
 
@@ -105,14 +106,11 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
 
-        if not isinstance(value, int):
+        if type(value) is not int:
 
             raise TypeError("x must be an integer")
-
         if value < 0:
-
             raise ValueError("x must be >= 0")
-
         self.__x = value
 
     @property
@@ -123,7 +121,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
 
-        if not isinstance(value, int):
+        if type(value) is not int:
 
             raise TypeError("y must be an integer")
 
