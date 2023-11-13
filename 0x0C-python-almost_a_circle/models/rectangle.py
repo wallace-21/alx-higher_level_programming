af9bbf5d2e202c7a1deef28a80a6963073b68bdc
wallace-21 <exnesswallace>
2023-11-13 22:50:57 +0200

    def update(self, *args): that assigns an argument to each attribute3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,25 +9,27 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ constructor with 2 arguments
 
+        """ constructor with 2 arguments
         Args:
             id: identifies instances of Base
         """
 
         if id is not None:
+
             self.id = id
+
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-
 class Rectangle(Base):
+
     """ a class Rectangle that inherits from Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ initialize an instance of Rectangle.
 
+        """ initialize an instance of Rectangle.
         Args:
             width: the width value
             height: the height vlaue
@@ -45,10 +47,6 @@ class Rectangle(Base):
         if not isinstance(x, int):
 
             raise TypeError("x must be an integer")
-
-        if not isinstance(y, int):
-
-            raise TypeError("y must be an integer")
 
         if y < 0:
 
@@ -69,12 +67,13 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+
+        if not isinstance(value, int):
 
             raise TypeError("width must be an integer")
 
-        if value <= 0:
-
+        if value <= 0
+        :
             raise ValueError("width must be > 0")
 
         self.__width = value
@@ -88,12 +87,11 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
 
-        if type(value) is not int:
+        if not isinstance(value, int):
 
             raise TypeError("height must be an integer")
 
         if value <= 0:
-
             raise ValueError("height must be > 0")
 
         self.__height = value
@@ -101,32 +99,39 @@ class Rectangle(Base):
     @property
     def x(self):
         """ getter for the x-coordinate attribute """
+
         return self.__x
 
     @x.setter
     def x(self, value):
 
-        if type(value) is not int:
+        if not isinstance(value, int):
 
             raise TypeError("x must be an integer")
+
         if value < 0:
+
             raise ValueError("x must be >= 0")
+
         self.__x = value
 
     @property
     def y(self):
         """ getter for the x-coordinate attribute """
+
         return self.__y
 
     @y.setter
     def y(self, value):
 
-        if type(value) is not int:
+        if not isinstance(value, int):
 
             raise TypeError("y must be an integer")
 
         if value < 0:
+
             raise ValueError("y must be >= 0")
+
         self.__y = value
 
     def area(self):
@@ -146,29 +151,8 @@ class Rectangle(Base):
             print(" " * self.x + "#" * self.width)
 
     def __str__(self):
+
         """ overriding the __str__ method """
 
         return ("[Rectangle] ({}) {}/{} - {}/{}"
                 .format(self.id, self.x, self.y, self.width, self.height))
-
-    def update(self, *args, **kwargs):
-
-        if len(args) >= 1:
-
-            self._id = args[0]
-
-        if len(args) >= 2:
-
-            self.__width = args[1]
-
-        if len(args) >= 3:
-
-            self.__height = args[2]
-
-        if len(args) >= 4:
-
-            self.__x = args[3]
-
-        if len(args) >= 5:
-
-            self.__x = args[4]
