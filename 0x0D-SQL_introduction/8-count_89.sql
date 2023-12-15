5fc76e9be624061129d3d9bdeebe3b7a156ac012
wallace <exnesswallace@gmail.com>
2023-12-15 02:25:10 +0200

    redo

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in my MySQL server
 SELECT COUNT(id)
-FROM table_name
+FROM first_table
 WHERE id = 89;
