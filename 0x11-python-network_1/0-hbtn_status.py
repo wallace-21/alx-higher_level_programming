5c8b35429486fd8587049254996275f6c5f1ec0a
wallace <exnesswallace@gmail.com>
2024-01-26 18:25:36 +0200

    removed tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as output:
         out = output.read()
         print("Body response:")
-        print("\t- type: {}".format(type(out)))
-        print("\t- content: {}".format(out))
+        print("    - type: {}".format(type(out)))
+        print("    - content: {}".format(out))
