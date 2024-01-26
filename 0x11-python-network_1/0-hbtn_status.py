d45d96bc7799a7020e704d0d412d7beff6b461ff
wallace <exnesswallace@gmail.com>
2024-01-26 18:23:15 +0200

    add main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,9 @@
 """import url library"""
 from urllib import request
 
-#if __name__ == "__main__":
-with request.urlopen("https://alx-intranet.hbtn.io/status") as output:
-    out = output.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(out)))
-    print("\t- content: {}".format(out))
+if __name__ == "__main__":
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as output:
+        out = output.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(out)))
+        print("\t- content: {}".format(out))
