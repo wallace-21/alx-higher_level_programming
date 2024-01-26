27b81884fd11bf3d09a9d95dcfc13c374d979ee1
wallace <exnesswallace@gmail.com>
2024-01-26 18:42:41 +0200

    added the decoded output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as output:
         out = output.read()
         print("Body response:")
-        print("    - type: {}".format(type(out)))
-        print("    - content: {}".format(out))
+        print("\t- type: {}".format(type(out)))
+        print("\t- content: {}".format(out))
+        print("\t- utf8 content: {}".format(out.decode('utf-8')))
