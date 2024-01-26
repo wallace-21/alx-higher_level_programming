913cc583075bdffb49c1280bef6285398605f780
wallace <exnesswallace@gmail.com>
2024-01-26 17:27:44 +0200

    reveted

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,9 @@
 
 from urllib import request
 
-if __name__ == "__main__":
-    with request.urlopen("https://alx-intranet.hbtn.io/status") as output:
-        out = output.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(out)))
-        print("\t- content: {}".format(out))
+#if __name__ == "__main__":
+with request.urlopen("https://alx-intranet.hbtn.io/status") as output:
+    out = output.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(out)))
+    print("\t- content: {}".format(out))
