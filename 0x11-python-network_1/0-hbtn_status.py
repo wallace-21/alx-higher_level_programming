a984cf885a91835c0080613fb880e3e103047183
wallace <exnesswallace@gmail.com>
2024-01-26 17:05:22 +0200

    added body response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,5 +5,6 @@ from urllib import request
 #if __name__ == "__main__":
 with request.urlopen("https://alx-intranet.hbtn.io/status") as output:
     out = output.read()
-    print("- type: {}".format(type(out)))
-    print("- content: {}".format(out))
+    print("Body response:")
+    print("    - type: {}".format(type(out)))
+    print("    - content: {}".format(out))
