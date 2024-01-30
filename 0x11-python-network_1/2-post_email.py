ee9256fcb24e7869277aa98527a378c7dba31f16
wallace <exnesswallace@gmail.com>
2024-01-30 15:11:19 +0200

    put everything under main

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,13 +5,13 @@ from urllib import request
 from urllib import parse
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
-my_data = {
-    "email": email
-}
 
 if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    my_data = {
+    "email": email
+}
     encode_email = parse.urlencode(my_data).encode('utf-8')
 
     req = request.Request(url, data=encode_email, method="POST")
