52c8be8b8508e6094eb742c58f1b4ea86eeff3cb
wallace <exnesswallace@gmail.com>
2024-01-30 15:14:23 +0200

    pycodestyle was missing

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     my_data = {
-    "email": email
-}
+        "email": email
+        }
     encode_email = parse.urlencode(my_data).encode('utf-8')
 
     req = request.Request(url, data=encode_email, method="POST")
 
     with request.urlopen(req) as output:
-            out = output.read().decode('utf-8')
-            print(out)
+        out = output.read().decode('utf-8')
+        print(out)
