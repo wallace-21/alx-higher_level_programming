d54420d47bdbd38a6b1c25d25c64559fac135af8
wallace <exnesswallace@gmail.com>
2024-02-20 17:01:13 +0200

    removed console.log form printing out what is passed

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,9 +4,7 @@ const fs = require('node:fs');
 const path = process.argv;
 
 fs.writeFile(path[2], path[3], 'utf-8', (error) => {
-  if (!error) {
+  if (error) {
     console.error(error);
-  } else {
-    console.log(path[2]);
   }
 });
