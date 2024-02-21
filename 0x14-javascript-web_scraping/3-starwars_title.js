b1539e7e851c6cbe45d306b49a281d4437892961
wallace <exnesswallace@gmail.com>
2024-02-21 02:52:40 +0200

    corrected spelling error

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,7 +7,7 @@ request(`https://swapi-api.hbtn.io/api/films/${id}`, (error, response, body) => 
   if (error) {
     console.error(error);
   } else if (response.statusCode === 200) {
-    const resp = JSON.parse(body);
+    const res = JSON.parse(body);
     console.log(res.title);
   }
 });
